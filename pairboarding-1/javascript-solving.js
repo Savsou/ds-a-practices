c15db8dd1620148dc43337331fd3f0c0edc647a0
savonnas <savonna.sou@gmail.com>
2025-02-05 22:46:12 -0500

    finished question 2 in pairboarding 2

--- pairboarding-1/javascript-solving.js
+++ pairboarding-1/javascript-solving.js
@@ -1,24 +1,69 @@
 //Solution 1:
 function findMissingNumber(arrayOfIntegers, upperBound, lowerBound) {
-    // const actualSum = arrayOfIntegers.reduce((acc, num) => acc + num, 0)
-    let sumOfInt = 0;
-    for (let i = 0; i < arrayOfIntegers.length; i++) {
-        sumOfInt += arrayOfIntegers[i]
-    }
+
+    const actualSum = arrayOfIntegers.reduce((acc, num) => acc + num, 0)
+    // let sumOfInt = 0;
+    // for (let i = 0; i < arrayOfIntegers.length; i++) {
+    //     sumOfInt += arrayOfIntegers[i]
+    // }
     const upperLimitSum = (upperBound * (upperBound + 1)) / 2;
     const lowerLimitSum = (lowerBound * (lowerBound - 1)) / 2;
 
     const theorySum = upperLimitSum - lowerLimitSum;
 
-    // const result = theorySum - actualSum;
-    const result = theorySum - sumOfInt;
+    const result = theorySum - actualSum;
+    // const result = theorySum - sumOfInt;
 
     return result
 };
 
 //Question 1:
-arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
-upperBound = 9;
-lowerBound = 1;
+// arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
+// upperBound = 9;
+// lowerBound = 1;
 
-console.log(findMissingNumber(arrayOfIntegers, upperBound, lowerBound)); // Output: 8
+// console.log(findMissingNumber(arrayOfIntegers, upperBound, lowerBound));
+// Output: 8
+
+
+//Solution 2:
+function findMagicIndex(array, start = 0, end = 7) {
+    // let ogMid = Math.floor((start + end) / 2);
+
+    if (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        console.log(mid);
+
+        if (array[mid] === mid) {
+            return mid;
+        }
+
+        //right now mid is 3
+        let leftSide = findMagicIndex(array, start, mid - 1)
+        if (leftSide !== -1) {
+            return leftSide;
+        }
+
+        // if (leftSide == -1) {
+        //     return findMagicIndex(array, mid - 1, end)
+        // } else {
+        //     return leftSide;
+        // }
+
+        let rightSide = findMagicIndex(array, mid + 1, end)
+        if (rightSide !== -1) {
+            return rightSide;
+        }
+    }
+
+    return -1
+}
+
+//Question 2:
+array1 = [-4, -2, 2, 6, 6, 6, 6, 10]
+array2 = [-4, -2, 1, 6, 6, 6, 6, 10]
+array3 = [-4, -2, 1, 6, 6, 6, 7, 10]
+
+// findMagicIndex(array1);
+console.log(findMagicIndex(array3))
