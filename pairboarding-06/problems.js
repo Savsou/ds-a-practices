e724c2681148c71d584ea476654c9a0a38667f7d
savonnas <savonna.sou@gmail.com>
2025-03-22 12:32:43 -0400

    add and finish problems 1 and 2 of the last section of pairboarding-06

--- pairboarding-06/problems.js
+++ pairboarding-06/problems.js
@@ -32,3 +32,134 @@ var hasCycle = function (head) {
 
     return false;
 };
+
+//need a tree node class to test these. Unable to find the questions in leetcode
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val, currentNode = this.root) {
+        const newNode = new TreeNode(val);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this.root;
+        }
+
+        if (val < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+            } else {
+                return this.insert(val, currentNode.left);
+            }
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+            } else {
+                return this.insert(val, currentNode.right);
+            }
+        }
+
+        return newNode;
+    }
+}
+
+const bst = new BST();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(8);
+bst.insert(2);
+bst.insert(4);
+bst.insert(12);
+bst.insert(20);
+bst.insert(30);
+
+//solution here
+function findSecondLargest(head) {
+    if (head.right) {
+        if (head.right.left || head.right.right) {
+            return findSecondLargest(head.right)
+        } else {
+            return head;
+        }
+    } else {
+        return findRightMostNode(head.left);
+    }
+}
+
+function findRightMostNode(node) {
+    if (node.right) {
+        return findRightMostNode(node.right)
+    } else {
+        return node;
+    };
+}
+
+console.log(findSecondLargest(bst.root).value); // Should return 20
+
+
+//question 2 backwardds addition
+class ListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+function addReverse(first_node, second_node, carry = 0) {
+    if (!first_node && !second_node && carry === 0) {
+        return null;
+    }
+
+    let sum = carry;
+    sum += first_node ? first_node.value : 0;
+    sum += second_node ? second_node.value : 0;
+
+    let newCarry = Math.floor(sum / 10);
+
+    sum %= 10
+    let newNode = new ListNode(sum)
+    newNode.next = addReverse(first_node ? first_node.next : null, second_node ? second_node.next : null, newCarry);
+
+    return newNode
+}
+
+//test, will create the linked list
+function createLinkedList(arr) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    for (let num of arr) {
+        current.next = new ListNode(num);
+        current = current.next;
+    }
+    return dummyHead.next;
+}
+
+//test, will print out the result of 5->4->8 as shown in example
+//not doing the action of reversing since addReverse is doing that
+//just pushing the values into the array and join them with -> between them
+function printLinkedList(head) {
+    let result = [];
+    while (head) {
+        result.push(head.value);
+        head = head.next;
+    }
+    console.log(result.join("->"));
+}
+
+let l1 = createLinkedList([6, 5]);    // Represents 56 (reversed)
+let l2 = createLinkedList([9, 8, 7]); // Represents 789 (reversed)
+
+let result = addReverse(l1, l2);
+printLinkedList(result); // Output: 5->4->8 (Represents 548)
