f3bc753f25f05fc1c664bcb487b8f56f9af90acb
savonnas <savonna.sou@gmail.com>
2025-04-18 12:32:26 -0400

    pivotIndex in leetcode75

--- leetcode75/prefix-sum/solving-prefix-sum.js
+++ leetcode75/prefix-sum/solving-prefix-sum.js
@@ -11,3 +11,21 @@ var largestAltitude = function (gain) {
 
     return highestAltitude
 };
+
+//https://leetcode.com/problems/find-pivot-index/submissions/1610692705/?envType=study-plan-v2&envId=leetcode-75
+
+var pivotIndex = function (nums) {
+    let totalSum = nums.reduce((acc, num) => acc + num, 0)
+    let leftSum = 0;
+
+    for (let pivot = 0; pivot < nums.length; pivot++) {
+        let rightSum = totalSum - leftSum - nums[pivot];
+        if (leftSum == rightSum) {
+            return pivot
+        }
+
+        leftSum += nums[pivot]
+    }
+
+    return -1
+};
