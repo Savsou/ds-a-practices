56ea1ece3a602397a3967cdc3179f3eb2e3a2697
savonnas <savonna.sou@gmail.com>
2025-05-06 19:28:28 -0400

    two pointers, added maxOperations

--- leetcode75/two-pointers/solvingPointers.js
+++ leetcode75/two-pointers/solvingPointers.js
@@ -46,3 +46,29 @@ var maxArea = function (height) {
 
     return maxArea
 };
+
+
+//https://leetcode.com/problems/max-number-of-k-sum-pairs/?envType=study-plan-v2&envId=leetcode-75
+var maxOperations = function (nums, k) {
+    nums.sort((a, b) => a - b);
+
+    let left = 0;
+    let right = nums.length - 1;
+    let operations = 0;
+
+    while (left < right) {
+        let currentSum = nums[left] + nums[right];
+
+        if (currentSum === k) {
+            operations += 1;
+            left += 1;
+            right -= 1;
+        } else if (currentSum < k) {
+            left += 1;
+        } else {
+            right -= 1;
+        }
+    }
+
+    return operations
+};
