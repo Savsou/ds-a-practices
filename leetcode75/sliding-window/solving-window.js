64d9fe8401893f792dc1a5174e845b1d305f0110
savonnas <savonna.sou@gmail.com>
2025-05-10 11:03:43 -0400

    sliding window

---
+++ leetcode75/sliding-window/solving-window.js
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75
+
+var findMaxAverage = function (nums, k) {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let max = sum;
+
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k]
+        max = Math.max(max, sum)
+    }
+
+    return max / k;
+};
