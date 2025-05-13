d067318a1c7b14679cf57e815a86a2773b65fc7d
savonnas <savonna.sou@gmail.com>
2025-05-13 19:44:08 -0400

    add maxVowels in sliding window

--- leetcode75/sliding-window/solving-window.js
+++ leetcode75/sliding-window/solving-window.js
@@ -16,3 +16,35 @@ var findMaxAverage = function (nums, k) {
 
     return max / k;
 };
+
+//https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/?envType=study-plan-v2&envId=leetcode-75
+
+var maxVowels = function (s, k) {
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+    let count = 0;
+    let maxCount = 0;
+
+    for (let i = 0; i < k; i++) {
+        if (vowels.has(s[i])) {
+            count++
+        }
+    }
+
+    if (count === k) {
+        return count
+    }
+
+    maxCount = Math.max(maxCount, count);
+
+    for (let i = k; i <= s.length; i++) {
+        if (vowels.has(s[i])) {
+            count++;
+        }
+        if (vowels.has(s[i - k])) {
+            count--;
+        }
+        maxCount = Math.max(maxCount, count);
+    }
+
+    return maxCount
+};
